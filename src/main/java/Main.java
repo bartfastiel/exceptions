0536ec968abaa4658080e089143622958671a4ba
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-03-02 17:38:12 +0100

    Example scenario

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Home-Office: Hunger!");
+        String pizza = orderPizzaFromRestaurant();
+        System.out.println("Home-Office: Ich esse " + pizza);
+    }
+
+    static String orderPizzaFromRestaurant() {
+        System.out.println("Restaurant: Bestellung erhalten");
+        String flour = getFlourOutOfFlourSack();
+        System.out.println("Restaurant: " + flour + " geholt");
+        return "Salami-Pizza aus " + flour;
+    }
+
+    static String getFlourOutOfFlourSack() {
+        return "Weizenmehl";
+    }
+}
---
+++ src/main/java/MausException.java
@@ -0,0 +1,2 @@
+public class MausException extends RuntimeException {
+}
