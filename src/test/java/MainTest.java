9ba81393b6ad15941bab247bf4587c45a0e5bf61
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-03-07 15:02:33 +0100

    Implement test, expect exception

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,17 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 class MainTest {
 
     @Test
     void whenGetFlour_thenThrowIOException() {
-        Main.getFlourOutOfFlourSack();
+        try {
+            Main.getFlourOutOfFlourSack();
+            Assertions.fail("schlecht: Exception nicht aufgetreten");
+        } catch (MausException e) {
+            // super: die Exception ist aufgetreten
+        }
     }
 }
