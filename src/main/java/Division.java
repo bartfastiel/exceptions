4113f6982bb587c1cfc7a3b3c97dc5169e600c78
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-15 10:59:03 +0200

    Code of live coding session

---
+++ src/main/java/Division.java
@@ -0,0 +1,8 @@
+public class Division {
+
+    public static void main(String[] args) {
+        System.out.println("Das Ergebnis lautet:");
+        int x = 0;
+        System.out.println(5 / x);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Home-Office: Hunger!");
@@ -5,14 +7,19 @@ class Main {
         System.out.println("Home-Office: Ich esse " + pizza);
     }
 
-    static String orderPizzaFromRestaurant() {
+    static String orderPizzaFromRestaurant()  {
         System.out.println("Restaurant: Bestellung erhalten");
-        String flour = getFlourOutOfFlourSack();
+        String flour = null;
+        try {
+            flour = getFlourOutOfFlourSack();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("Restaurant: " + flour + " geholt");
         return "Salami-Pizza aus " + flour;
     }
 
-    static String getFlourOutOfFlourSack() {
-        throw new MausException();
+    static String getFlourOutOfFlourSack() throws IOException {
+        throw new IOException();
     }
 }
